0d7be535d8971236342f706c83767c2693384f40
David Ferguson <deejai@live.com>
2022-08-12 20:07:41 -0600

    tiny drag n drop demo

--- src/phaser/scene.js
+++ src/phaser/scene.js
@@ -5,20 +5,41 @@ class playGame extends Phaser.Scene {
   constructor() {
     super("PlayGame");
   }
+
   preload() {
     this.load.image("logo", logoImg);
+    this.dragOffset = {x: 0, y: 0};
   }
-  create() {
-    const logo = this.add.image(400, 150, "logo");
 
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: "Power2",
-      yoyo: true,
-      loop: -1
-    });
+  create() {
+    this.add.image(
+      this.game.config.width * .5,
+      this.game.config.height * .5,
+      "dot"
+    ).setInteractive();
+    this.input.on('pointerdown', this.startDrag, this);
+  }
+
+  startDrag(pointer, targets) {
+    this.input.off('pointerdown', this.startDrag, this);
+    this.dragObj = targets[0];
+    if(this.dragObj) {
+      this.dragOffset.x = this.dragObj.x - pointer.x;
+      this.dragOffset.y = this.dragObj.y - pointer.y;
+      this.input.on('pointermove', this.doDrag, this);
+      this.input.on('pointerup', this.stopDrag, this);
+    }
+  }
+
+  doDrag(pointer) {
+    this.dragObj.x = pointer.x + this.dragOffset.x;
+    this.dragObj.y = pointer.y + this.dragOffset.y;
+  }
+
+  stopDrag() {
+    this.input.on('pointerdown', this.startDrag, this);
+    this.input.off('pointermove', this.doDrag, this);
+    this.input.off('pointerup', this.stopDrag, this);
   }
 }
 
